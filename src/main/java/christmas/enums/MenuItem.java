36788d6f99efc262d31125aa5749f292dd005f3f
Yong Ta Park <qkrdydxk80@nate.com>
2023-11-09 23:56:36 +0900

    refactor : 해시맵 인스턴스 제거, getByName 메서드 제거

--- src/main/java/christmas/enums/MenuItem.java
+++ src/main/java/christmas/enums/MenuItem.java
@@ -5,12 +5,6 @@ import java.util.HashMap;
 import java.util.Map;
 
 public interface MenuItem {
-    Map<String,MenuItem> NAME_TO_ITEM_MAP = new HashMap<>();
     String getName();
     Integer getPrice();
-
-     static MenuItem getByName(String name){
-        return NAME_TO_ITEM_MAP.get(name);
-    }
-
 }
