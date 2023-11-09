f66983158b1aa6336892d32d4ef8f8443dbe63d9
Yong Ta Park <qkrdydxk80@nate.com>
2023-11-09 23:59:26 +0900

    feat : 메뉴리스트들을 저장하도록 구현

---
+++ src/main/java/christmas/enums/MenuList.java
@@ -0,0 +1,33 @@
+package christmas.enums;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MenuList {
+    private static final Map<String, MenuItem> menuItems = new HashMap<>();
+
+    static {
+        for(AppetizerMenu appetizerMenu : AppetizerMenu.values()){
+            menuItems.put(appetizerMenu.getName(),appetizerMenu);
+        }
+
+        for(BeverageMenu beverageMenu : BeverageMenu.values()){
+            menuItems.put(beverageMenu.getName(),beverageMenu);
+        }
+
+        for(DessertMenu dessertMenu : DessertMenu.values()){
+            menuItems.put(dessertMenu.getName(),dessertMenu);
+        }
+
+        for(MainMenu mainMenu : MainMenu.values()){
+            menuItems.put(mainMenu.getName(),mainMenu);
+        }
+    }
+
+    private MenuList() {
+    }
+
+    public static MenuItem getMenuByName(String name){
+        return menuItems.get(name);
+    }
+}
