e6c665b6199e212b5688839166ebc358ebdc5e9d
Yong Ta Park <qkrdydxk80@nate.com>
2023-11-10 13:40:51 +0900

    test : 이벤트 기한이 지나면 할인가 미적용 검증

--- src/test/java/christmas/event/ChristmasDDayDiscountTest.java
+++ src/test/java/christmas/event/ChristmasDDayDiscountTest.java
@@ -15,16 +15,18 @@ class ChristmasDDayDiscountTest {
 
     private final static LocalDate startDate = LocalDate.of(2023, Month.DECEMBER, 1);
     private final static LocalDate endDate = LocalDate.of(2023, Month.DECEMBER, 25);
+    private final static LocalDate overDate = LocalDate.of(2023, Month.DECEMBER, 26);
     private final static ChristmasDDayDiscount christmasDDayDiscount = new ChristmasDDayDiscount(startDate, endDate);
     private final static Integer DISCOUNT_START_AMOUNT = 1000;
     private final static Integer DISCOUNT_INCREMENT_PER_DAY = 100;
 
-    @DisplayName("날짜를 입력했을 때, 이벤트 날짜에 해당되면 할인금액을 반환한다.")
+    @DisplayName("날짜를 입력했을 때, 이벤트 날짜에 해당되지 않으면 0을 반환한다.")
     @Test
     void whenEventActivate() {
-        Integer discountAmount = christmasDDayDiscount.executeEvent(LocalDate.of(2023, Month.DECEMBER, 7));
 
-        assertThat(discountAmount).isEqualTo(1600);
+        Integer discountAmount = christmasDDayDiscount.executeEvent(overDate);
+
+        assertThat(discountAmount).isEqualTo(0);
     }
 
     @ParameterizedTest
