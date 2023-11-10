b500581c43768a071b7af9b1519f2854c309eec7
Yong Ta Park <qkrdydxk80@nate.com>
2023-11-10 19:45:56 +0900

    feat : 특정일, 특정월, 특정주 기간 생성 기능 구현

--- src/main/java/christmas/event/EventPeriod.java
+++ src/main/java/christmas/event/EventPeriod.java
@@ -1,9 +1,32 @@
 package christmas.event;
 
+import java.time.DayOfWeek;
 import java.time.LocalDate;
+import java.time.Month;
+import java.time.temporal.TemporalAdjusters;
 
 public record EventPeriod(LocalDate startDate, LocalDate endDate) {
-    /*public static EventPeriod createOneMonthPeriod(){
+    //한달의 기한을 잡는다.
+    public static EventPeriod createMonthPeriod(int year, int month) {
+        LocalDate firstDayOfMonth = LocalDate.of(year, Month.of(month), 1);
+        LocalDate lastDayOfMonth = firstDayOfMonth.with(TemporalAdjusters.lastDayOfMonth());
+        return new EventPeriod(firstDayOfMonth, lastDayOfMonth);
+    }
 
-    }*/ // TODO:구현필
+    //특정 기한을 잡는다
+    public static EventPeriod createTypicalPeriod(int year, int month, int startDate, int endDate) {
+        LocalDate firstDayOfMonth = LocalDate.of(year, Month.of(month), startDate);
+        LocalDate lastDayOfMonth = LocalDate.of(year, Month.of(month), endDate);
+        return new EventPeriod(firstDayOfMonth, lastDayOfMonth);
+    }
+
+    //특정일부터 N주까지의 기한을 잡는다
+    public static EventPeriod createTypicalWeekPeriod(int year, int month, int startDate, int weekCount) {
+        final int perWeek = DayOfWeek.values().length;
+        int NWeekAfterDay = perWeek * weekCount;
+
+        LocalDate firstDayOfMonth = LocalDate.of(year, Month.of(month), startDate);
+        LocalDate lastDayOfMonth = LocalDate.of(year, Month.of(month), NWeekAfterDay);
+        return new EventPeriod(firstDayOfMonth, lastDayOfMonth);
+    }
 }
