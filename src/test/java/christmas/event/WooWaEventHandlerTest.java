67849c22bd60d11ac44d38493040e857722d67fd
Yong Ta Park <qkrdydxk80@nate.com>
2023-11-11 00:22:55 +0900

    test : 12만원 이상,이하 경우 검증

--- src/test/java/christmas/event/WooWaEventHandlerTest.java
+++ src/test/java/christmas/event/WooWaEventHandlerTest.java
@@ -1,5 +1,6 @@
 package christmas.event;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 import christmas.enums.menu.DessertMenu;
@@ -11,6 +12,8 @@ import christmas.order.Orders;
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class WooWaEventHandlerTest {
@@ -19,17 +22,30 @@ class WooWaEventHandlerTest {
     private final static OrderMenu orderMenuWithMain = new OrderMenu(MainMenu.T_BONE_STEAK, 2);
     private final static Orders ordersWithDessert = new Orders(List.of(orderMenuWithDessert));
     private final static Orders ordersWithMain = new Orders(List.of(orderMenuWithMain));
+    private final static Orders ordersOver120_000 = new Orders(List.of(orderMenuWithDessert,orderMenuWithMain));
+    private final static LocalDate reservationDate = LocalDate.of(2023, Month.DECEMBER, 3);
 
 
+
+    @DisplayName("특별할인, 주중할인(2), 증정이벤트, 크리스마스할인 당첨시")
     @Test
-    void activateEvent() {
+    void whenSpecialChristmasWeekdayGiftEvent() {
+        final Integer totalBenefitAmount = 1000 + 1200 + (2023*2) + 25000;
         WooWaEventHandler wooWaEventHandler = new WooWaEventHandler();
-        EventBenefit eventBenefit = wooWaEventHandler.activateEvent(LocalDate.of(2023, Month.DECEMBER, 3),
-                ordersWithMain);
-        Integer integer = eventBenefit.discountBenefit();
-        System.out.println("integer = " + integer);
-        MenuItem gift = eventBenefit.gift();
-        System.out.println("gift = " + gift);
+        EventBenefit eventBenefit = wooWaEventHandler.activateEvent(reservationDate, ordersOver120_000);
+        Integer totalBenefit = eventBenefit.discountBenefit();
+        assertThat(totalBenefit).isEqualTo(totalBenefitAmount);
+
+    }
+
+    @DisplayName("특별할인, 주중할인(2), 증정이벤트, 크리스마스할인 당첨시")
+    @Test
+    void whenSpecialChristmasWeekdayEvent() {
+        final Integer totalBenefitAmount = 1000 + 1200 + (2023*2);
+        WooWaEventHandler wooWaEventHandler = new WooWaEventHandler();
+        EventBenefit eventBenefit = wooWaEventHandler.activateEvent(reservationDate, ordersWithMain);
+        Integer totalBenefit = eventBenefit.discountBenefit();
+        assertThat(totalBenefit).isEqualTo(totalBenefitAmount);
 
     }
 }
