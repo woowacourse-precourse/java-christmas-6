2f264ff2d7e31f1b6ee89a3ca2eb94868650b58b
Yong Ta Park <qkrdydxk80@nate.com>
2023-11-10 19:02:29 +0900

    feat : 시작일과 종료일을 감싸는 래퍼클래스

---
+++ src/main/java/christmas/event/EventPeriod.java
@@ -0,0 +1,9 @@
+package christmas.event;
+
+import java.time.LocalDate;
+
+public record EventPeriod(LocalDate startDate, LocalDate endDate) {
+    /*public static EventPeriod createOneMonthPeriod(){
+
+    }*/ // TODO:구현필
+}
