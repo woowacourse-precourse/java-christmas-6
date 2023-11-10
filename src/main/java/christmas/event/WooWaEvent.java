dbe47afde2f740e324bf887d8c9aff54042d101d
Yong Ta Park <qkrdydxk80@nate.com>
2023-11-10 18:21:12 +0900

    refactor : default 메서드 제거, 하위 인터페이스에 메서드 위임

--- src/main/java/christmas/event/WooWaEvent.java
+++ src/main/java/christmas/event/WooWaEvent.java
@@ -7,21 +7,4 @@ public interface WooWaEvent {
 
     Boolean isEventActivate(LocalDate reservationDate);
 
-    default Integer executeDateToDateEvent(LocalDate reservationDate){
-        return null;
-    }
-
-    default Integer executePerMenuDiscountEvent(LocalDate reservationDate, OrderMenu orderMenu){
-        return null;
-    }
-
-    default Boolean executeGiftEvent(LocalDate reservationDate, Integer totalOrderAmount){
-        return null;
-    }
-
-    default Integer executeDateDiscountEvent(LocalDate reservationDate){
-        return null;
-    }
-
-
 }
