22bf0a03e71b68d54f438f6759ff935f6dd1864a
Yong Ta Park <qkrdydxk80@nate.com>
2023-11-10 14:49:32 +0900

    refactor : execute 메서드 default 오버라이딩

--- src/main/java/christmas/event/WooWaEvent.java
+++ src/main/java/christmas/event/WooWaEvent.java
@@ -1,10 +1,19 @@
 package christmas.event;
 
+import christmas.order.OrderMenu;
 import java.time.LocalDate;
 
 public interface WooWaEvent {
 
     Boolean isEventActivate(LocalDate reservationDate);
 
-    Integer executeEvent(LocalDate reservationDate);
+    default Integer executeEvent(LocalDate reservationDate){
+        return null;
+    }
+
+    default Integer executeEvent(LocalDate reservationDate,OrderMenu orderMenu){
+        return null;
+    }
+
+
 }
