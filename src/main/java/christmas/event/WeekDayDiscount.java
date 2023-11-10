ac6b395e8ca6ede036d0bd38fb748b92be91b3f8
Yong Ta Park <qkrdydxk80@nate.com>
2023-11-10 14:48:31 +0900

    feat : 주중할인 구현

---
+++ src/main/java/christmas/event/WeekDayDiscount.java
@@ -0,0 +1,46 @@
+package christmas.event;
+
+import christmas.enums.DessertMenu;
+import christmas.enums.MenuItem;
+import christmas.order.OrderMenu;
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+
+public class WeekDayDiscount implements WooWaEvent {
+    private final static Integer DISCOUNT_AMOUNT = 2023;
+    private final LocalDate startDate;
+    private final LocalDate endDate;
+
+    public WeekDayDiscount(LocalDate startDate, LocalDate endDate) {
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+
+    public int calculateDiscount(OrderMenu orderMenu) {
+        if (isDessert(orderMenu.getMenuItem())) {
+           return orderMenu.getOrderQuantity() * DISCOUNT_AMOUNT;
+        }
+        return 0;
+    }
+    private boolean isDessert(MenuItem menuItem) {
+        return menuItem instanceof DessertMenu;
+    }
+
+    private Boolean isWeekDay(LocalDate reservationDate) {
+        DayOfWeek dayOfWeek = reservationDate.getDayOfWeek();
+        return (dayOfWeek != DayOfWeek.SATURDAY && dayOfWeek != DayOfWeek.SUNDAY);
+    }
+
+    @Override
+    public Boolean isEventActivate(LocalDate reservationDate) {
+        return !(reservationDate.isBefore(startDate) || reservationDate.isAfter(endDate));
+    }
+
+    @Override
+    public Integer executeEvent(LocalDate reservationDate,OrderMenu orderMenu) {
+        if(isEventActivate(reservationDate) && isWeekDay(reservationDate)){
+            return calculateDiscount(orderMenu);
+        }
+        return 0;
+    }
+}
