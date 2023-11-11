fda7db3387b2e6812fde24e5724659bfee49ee29
Yong Ta Park <qkrdydxk80@nate.com>
2023-11-11 16:19:22 +0900

    feat : 이벤트, 뱃지 흐름 조절

---
+++ src/main/java/christmas/order/OrderSystem.java
@@ -0,0 +1,33 @@
+package christmas.order;
+
+import christmas.enums.badge.benefit.BenefitBadge;
+import christmas.enums.menu.MenuItem;
+import christmas.event.EventBenefit;
+import christmas.manangers.BadgeManager;
+import christmas.manangers.WooWaEventManager;
+import java.time.LocalDate;
+import java.util.List;
+
+public class OrderSystem {
+    private final WooWaEventManager wooWaEventManager;
+    private final BadgeManager badgeManager;
+
+    public OrderSystem(WooWaEventManager wooWaEventManager, BadgeManager badgeManager) {
+        this.wooWaEventManager = wooWaEventManager;
+        this.badgeManager = badgeManager;
+    }
+
+    public Receipt orderProcess(LocalDate reservationDate, List<Order> unprocessedOrders){
+        Orders orders = new Orders(unprocessedOrders);
+        Integer totalPriceBeforeDiscount = orders.calculateTotalPrice();
+
+        EventBenefit eventBenefit = wooWaEventManager.activateEvent(reservationDate, orders);
+        Integer discountBenefit = eventBenefit.discountBenefit();
+        MenuItem gift = eventBenefit.gift();
+
+        BenefitBadge badge = badgeManager.isBadgeConditionSatisfied(discountBenefit);
+
+        return new Receipt(orders,totalPriceBeforeDiscount,discountBenefit,gift,badge);
+    }
+
+}
