a9d299d284165f511592c14554976f9006381ceb
Yong Ta Park <qkrdydxk80@nate.com>
2023-11-11 13:00:57 +0900

    refactor : 파라미터 MenuItem -> MenuItem[] 으로 변경

--- src/main/java/christmas/order/Orders.java
+++ src/main/java/christmas/order/Orders.java
@@ -1,5 +1,6 @@
 package christmas.order;
 
+import christmas.enums.menu.MainMenu;
 import christmas.enums.menu.MenuItem;
 import java.util.List;
 
@@ -14,9 +15,8 @@ public class Orders {
         return orderMenuList.stream().mapToInt(OrderMenu::calculatePrice).sum();
     }
 
-    public Integer findEventMenuCount(MenuItem menuItem) {
-        return orderMenuList.stream().mapToInt(menu -> menu.findEventMenuCount(menuItem)).sum();
-
+    public Integer findEventMenuCount(MenuItem[] menuItems) {
+        return orderMenuList.stream().mapToInt(menu -> menu.findEventMenuCount(menuItems)).sum();
     }
 
 }
