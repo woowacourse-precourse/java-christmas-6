81f8b3daf246ade363bce1cafc9265648ae09674
Yong Ta Park <qkrdydxk80@nate.com>
2023-11-11 16:15:22 +0900

    feat : 숫자 금액으로 포맷팅 구현

---
+++ src/main/java/christmas/views/NumberFormatter.java
@@ -0,0 +1,10 @@
+package christmas.views;
+
+import java.text.NumberFormat;
+
+public class NumberFormatter {
+    private final static NumberFormat format = NumberFormat.getNumberInstance();
+    public static String formatting(int number){
+        return format.format(number);
+    }
+}
