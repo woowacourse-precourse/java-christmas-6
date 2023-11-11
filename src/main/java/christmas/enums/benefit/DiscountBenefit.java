3c089d11d581ee09bbc513dc5f12dade7442d3f5
Yong Ta Park <qkrdydxk80@nate.com>
2023-11-11 11:12:48 +0900

    feat : 혜택금액 enum 구현

---
+++ src/main/java/christmas/enums/benefit/DiscountBenefit.java
@@ -0,0 +1,19 @@
+package christmas.enums.benefit;
+
+public enum DiscountBenefit {
+    BASIC_BENEFIT(1_000)
+    , INCREASE_BENEFIT(100)
+    , WEEK_BENEFIT(2_023)
+    , GIFT_CONDITION_BENEFIT(120_000)
+    , NO_BENEFIT(0);
+    
+    private final Integer amount;
+
+    DiscountBenefit(Integer amount) {
+        this.amount = amount;
+    }
+
+    public Integer getAmount() {
+        return amount;
+    }
+}
