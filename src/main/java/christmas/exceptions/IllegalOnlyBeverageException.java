2fd7f4a0c0c95043ec61693942218e06225f4d2b
Yong Ta Park <qkrdydxk80@nate.com>
2023-11-12 11:19:53 +0900

    feat : 음료만 선택했을 시 예외

---
+++ src/main/java/christmas/exceptions/IllegalOnlyBeverageException.java
@@ -0,0 +1,10 @@
+package christmas.exceptions;
+
+public class IllegalOnlyBeverageException extends RestaurantException{
+
+    private final static String DEFAULT_MESSAGE = "죄송합니다. 음료만 주문하실 수는 없습니다. 다른 메뉴도 함께 주문하시는것은 어떠실까요?";
+
+    public IllegalOnlyBeverageException() {
+        super(DEFAULT_MESSAGE);
+    }
+}
--- src/main/java/christmas/utils/StringToOrdersParser.java
+++ src/main/java/christmas/utils/StringToOrdersParser.java
@@ -1,6 +1,9 @@
 package christmas.utils;
 
+import christmas.enums.menu.BeverageMenu;
+import christmas.exceptions.IllegalOnlyBeverageException;
 import christmas.exceptions.IllegalOrderFormatException;
+import christmas.exceptions.IllegalOutOfNumberMenuException;
 import christmas.order.Order;
 import java.util.HashSet;
 import java.util.Set;
@@ -13,7 +16,21 @@ public class StringToOrdersParser {
         for (String menu : inputs) {
             parseSingleOrderFromInput(menuSet, menu);
         }
+        isNotOnlyBeverage(menuSet);
+        isQuantityNotOver20(menuSet);
         return menuSet;
+    }
+
+    private static void isQuantityNotOver20(Set<Order> menuSet) {
+        if (menuSet.stream().mapToInt(Order::getOrderQuantity).sum() > 20) {
+            throw new IllegalOutOfNumberMenuException();
+        }
+    }
+
+    private static void isNotOnlyBeverage(Set<Order> menuSet) {
+        if (menuSet.stream().allMatch(order -> order.getMenuItem() instanceof BeverageMenu)) {
+            throw new IllegalOnlyBeverageException();
+        }
     }
 
     private static void parseSingleOrderFromInput(Set<Order> menuList, String menu) {
@@ -26,13 +43,27 @@ public class StringToOrdersParser {
 
     private static void createOrderFromMenuItem(Set<Order> menuSet, String menu) {
         String[] menuAndQuantity = menu.split("-");
+        isCorrectForm(menuAndQuantity);
+        String menuItem = menuAndQuantity[0].trim();
+        int quantity = Integer.parseInt(menuAndQuantity[1].trim());
+        isCorrectQuantity(quantity);
+        Order order = new Order(MenuList.getMenuByName(menuItem), quantity);
+        isExistMenu(menuSet, order);
+    }
+
+    private static void isCorrectForm(String[] menuAndQuantity) {
         if (menuAndQuantity.length != 2) {
             throw new IllegalOrderFormatException();
         }
-        String menuItem = menuAndQuantity[0].trim();
-        String quantity = menuAndQuantity[1].trim();
-        Order order = new Order(MenuList.getMenuByName(menuItem),
-                Integer.parseInt(quantity));
+    }
+
+    private static void isCorrectQuantity(int quantity) {
+        if (quantity == 0) {
+            throw new IllegalOrderFormatException();
+        }
+    }
+
+    private static void isExistMenu(Set<Order> menuSet, Order order) {
         if (!menuSet.add(order)) {
             throw new IllegalOrderFormatException();
         }
