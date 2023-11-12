68341e889bfc086f20fc7bf58c22ebf7ee304086
Yong Ta Park <qkrdydxk80@nate.com>
2023-11-12 14:52:08 +0900

    refactor : 반환형을 String 대신 ItemMenu 자체로 변경

--- src/main/java/christmas/event/gift/AmountToGiftEvent.java
+++ src/main/java/christmas/event/gift/AmountToGiftEvent.java
@@ -1,10 +1,11 @@
 package christmas.event.gift;
 
+import christmas.enums.menu.MenuItem;
 import christmas.event.WooWaEvent;
 import christmas.order.Orders;
 import java.time.LocalDate;
 
 public interface AmountToGiftEvent extends WooWaEvent {
-    String execute(LocalDate reservationDate, Integer totalPriceBeforeDiscount);
+    MenuItem execute(LocalDate reservationDate, Integer totalPriceBeforeDiscount);
 
 }
