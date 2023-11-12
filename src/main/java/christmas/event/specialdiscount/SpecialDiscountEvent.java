17c812e4603ed8fe5540585fc12146e2dd9a5282
Yong Ta Park <qkrdydxk80@nate.com>
2023-11-12 16:48:08 +0900

    refactor : return = Integer -> EventResult

--- src/main/java/christmas/event/specialdiscount/SpecialDiscountEvent.java
+++ src/main/java/christmas/event/specialdiscount/SpecialDiscountEvent.java
@@ -1,9 +1,11 @@
 package christmas.event.specialdiscount;
 
+import christmas.enums.events.Events;
+import christmas.event.EventResult;
 import christmas.event.WooWaEvent;
 import java.time.LocalDate;
 
 public interface SpecialDiscountEvent extends WooWaEvent {
-    Integer execute(LocalDate reservationDate);
+    EventResult execute(LocalDate reservationDate);
 
 }
--- src/main/java/christmas/event/weekdiscount/WeekDiscountEvent.java
+++ src/main/java/christmas/event/weekdiscount/WeekDiscountEvent.java
@@ -1,9 +1,10 @@
 package christmas.event.weekdiscount;
 
+import christmas.event.EventResult;
 import christmas.event.WooWaEvent;
 import christmas.order.Orders;
 import java.time.LocalDate;
 
 public interface WeekDiscountEvent extends WooWaEvent {
-    Integer execute(LocalDate reservationDate, Orders orders);
+    EventResult execute(LocalDate reservationDate, Orders orders);
 }
