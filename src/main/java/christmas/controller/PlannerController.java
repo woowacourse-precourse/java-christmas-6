f879945fa2b3327a9c73e8067e56a6b979b3a491
win9 <krrr883@gmail.com>
2023-11-13 22:19:39 +0900

    [Feat]: 메뉴 입력
    메뉴와 갯수를 입력 후 이를 저장

--- src/main/java/christmas/controller/PlannerController.java
+++ src/main/java/christmas/controller/PlannerController.java
@@ -5,6 +5,8 @@ import christmas.service.PlannerService;
 import christmas.view.InputView;
 import christmas.view.OutputView;
 
+import java.util.Map;
+
 public class PlannerController {
     private PlannerService plannerService = new PlannerService();
 
@@ -19,6 +21,15 @@ public class PlannerController {
             return plannerService.setVisitedDate(date);
         } catch (IllegalArgumentException e) {
             return dateInit();
+        }
+    }
+
+    private void menuInit() {
+        try {
+            Map<String, Integer> menuMap = InputView.printMenuInitInputMessage();
+            plannerService.setOrder(menuMap);
+        } catch (IllegalArgumentException e) {
+            menuInit();
         }
     }
 }
--- src/main/java/christmas/service/PlannerService.java
+++ src/main/java/christmas/service/PlannerService.java
@@ -1,11 +1,21 @@
 package christmas.service;
 
 import christmas.domain.Customer;
+import christmas.domain.Order;
+
+import java.util.Map;
 
 public class PlannerService {
     private Customer customer;
+    private Order order = new Order();
     public Customer setVisitedDate(int date) {
         customer = new Customer(date);
         return customer;
+    }
+
+    public void setOrder(Map<String, Integer> menuMap) {
+        for (String foodName : menuMap.keySet()) {
+            order.addOrder(foodName, menuMap.get(foodName));
+        }
     }
 }
