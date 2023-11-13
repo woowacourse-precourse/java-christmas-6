0eff739f8d295924eda521456d3516e41c35fea7
Yong Ta Park <qkrdydxk80@nate.com>
2023-11-13 16:46:32 +0900

    add : 상품이 당첨되지 않았을 경우를 반환

--- src/main/java/christmas/event/evnets/gift/AmountToAGiftEvent.java
+++ src/main/java/christmas/event/evnets/gift/AmountToAGiftEvent.java
@@ -2,22 +2,22 @@ package christmas.event.evnets.gift;
 
 import static christmas.enums.menu.NoMenu.NO_MENU;
 
-import christmas.enums.events.Events;
 import christmas.enums.menu.MenuItem;
+import christmas.event.Gift;
 import christmas.utils.EventPeriod;
 import java.time.LocalDate;
 
 public class AmountToAGiftEvent implements AmountToGiftEvent {
-    private final Events event;
     private final Integer needAmountToGift;
     private final EventPeriod eventPeriod;
     private final MenuItem menuItem;
+    private final Integer quantity;
 
-    public AmountToAGiftEvent(Events event, EventPeriod eventPeriod, Integer needAmountToGift, MenuItem menuItem) {
-        this.event = event;
+    public AmountToAGiftEvent(EventPeriod eventPeriod, Integer needAmountToGift, MenuItem menuItem, Integer quantity) {
         this.eventPeriod = eventPeriod;
         this.needAmountToGift = needAmountToGift;
         this.menuItem = menuItem;
+        this.quantity = quantity;
     }
 
     public MenuItem isGiftAmountOver(Integer totalOrderAmount) {
@@ -26,16 +26,21 @@ public class AmountToAGiftEvent implements AmountToGiftEvent {
         }
         return NO_MENU;
     }
+
+    public Boolean isMenuExist(MenuItem menuItem){
+        return !menuItem.isNone();
+    }
     @Override
     public Boolean isEventActivate(LocalDate reservationDate) {
         return !(reservationDate.isBefore(eventPeriod.startDate()) || reservationDate.isAfter(eventPeriod.endDate()));
     }
 
     @Override
-    public MenuItem execute(LocalDate reservationDate, Integer totalPriceBeforeDiscount) {
-        if (isEventActivate(reservationDate)) {
-            return isGiftAmountOver(totalPriceBeforeDiscount);
+    public Gift execute(LocalDate reservationDate, Integer totalPriceBeforeDiscount) {
+        if (isEventActivate(reservationDate) && menuItem.isNone()) {
+            MenuItem findMenuItem = isGiftAmountOver(totalPriceBeforeDiscount);
+            return new Gift(findMenuItem,quantity);
         }
-        return NO_MENU;
+        return Gift.NO_GIFT();
     }
 }
