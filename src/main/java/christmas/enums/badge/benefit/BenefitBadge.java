fe9849ea3ed8a83341ba24152e35e88c564b4887
Yong Ta Park <qkrdydxk80@nate.com>
2023-11-13 15:19:54 +0900

    refactor : 뱃지 여부가 enum의 순서에 영향을 받지 않도록 구현

--- src/main/java/christmas/enums/badge/benefit/BenefitBadge.java
+++ src/main/java/christmas/enums/badge/benefit/BenefitBadge.java
@@ -3,22 +3,24 @@ package christmas.enums.badge.benefit;
 import christmas.enums.badge.Badge;
 
 public enum BenefitBadge implements Badge {
-    SANTA("산타",20_000)
-    , TREE("트리",10_000)
-    , STAR("별",5_000)
-    , NONE("없음",0);
+    SANTA("산타",20_000, Integer.MAX_VALUE)
+    , TREE("트리",10_000, 19_999)
+    , STAR("별",5_000, 9_999)
+    , NONE("없음",0, 4_999);
 
     private final String name;
-    private final Integer benefitCondition;
+    private final Integer minimumBenefit;
+    private final Integer maxBenefit;
 
-    BenefitBadge(String name, Integer benefitCondition) {
+    BenefitBadge(String name, Integer minimumBenefit, Integer maxBenefit) {
         this.name = name;
-        this.benefitCondition = benefitCondition;
+        this.minimumBenefit = minimumBenefit;
+        this.maxBenefit = maxBenefit;
     }
 
-    public static BenefitBadge determineBadge(Integer totalBenefitAmount) {
-        for (BenefitBadge badge : values()) {
-            if (totalBenefitAmount >= badge.benefitCondition) {
+    public static BenefitBadge determineBadge(int totalBenefitAmount) {
+        for (BenefitBadge badge : BenefitBadge.values()) {
+            if (totalBenefitAmount >= badge.minimumBenefit && totalBenefitAmount <= badge.maxBenefit) {
                 return badge;
             }
         }
@@ -32,6 +34,6 @@ public enum BenefitBadge implements Badge {
 
     @Override
     public Integer getAmount() {
-        return benefitCondition;
+        return minimumBenefit;
     }
 }
