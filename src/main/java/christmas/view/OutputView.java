93746375e5cbb30fb048d238d736b13b947f578a
win9 <krrr883@gmail.com>
2023-11-14 02:30:33 +0900

    [Feat]: 미리보기 콘솔로직 추가

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -3,7 +3,14 @@ package christmas.view;
 import christmas.type.TextType;
 
 public class OutputView {
+
+    public static final String ENTER = "\n";
+
     public static void printStartMessageOutputMessage() {
         System.out.println(TextType.START_MESSAGE.getText());
+    }
+
+    public static void printPreviewOutputMessage(int date) {
+        System.out.println("12월 " + date + TextType.SHOW_BENEFIT_OUTPUT_MESSAGE + ENTER);
     }
 }
