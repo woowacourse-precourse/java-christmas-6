f8358c09c844b65f79694ac066949bd03ece16b0
win9 <krrr883@gmail.com>
2023-11-13 22:19:11 +0900

    [Feat]: 메뉴입력 분리
    메뉴를 입력받고 이를 검증 후 리스트로 변환

--- src/main/java/christmas/utils/Parser.java
+++ src/main/java/christmas/utils/Parser.java
@@ -1,6 +1,9 @@
 package christmas.utils;
 
+import java.util.Arrays;
+import java.util.Map;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class Parser {
     public static int convertToInt(String input) {
@@ -8,5 +11,15 @@ public class Parser {
             throw new IllegalArgumentException();
         }
         return Integer.parseInt(input);
+    }
+
+    public static Map<String, Integer> convertToMap(String input) {
+        if (!Pattern.matches("^([a-zA-Z0-9가-힣]+\\-[0-9]+,)*([a-zA-Z0-9가-힣]+\\-[0-9]+)$", input)) {
+            throw new IllegalArgumentException();
+        }
+
+        return Arrays.stream(input.split(","))
+                .map(s -> s.split("-"))
+                .collect(Collectors.toMap(a -> a[0], a -> Integer.parseInt(a[1])));
     }
 }
