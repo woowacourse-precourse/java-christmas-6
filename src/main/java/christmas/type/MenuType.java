394bca7682850a995cb6a1f706917429c2c6fedd
win9 <krrr883@gmail.com>
2023-11-13 18:59:40 +0900

    [Feat]: 음식타입 분리
    음식타입을 enum으로 분리

--- src/main/java/christmas/type/MenuType.java
+++ src/main/java/christmas/type/MenuType.java
@@ -1,29 +1,29 @@
 package christmas.type;
 
 public enum MenuType {
-    SOUP("애피타이저", "양송이수프", 6000),
-    TAPAS("애피타이저", "타파스", 5000),
-    SALAD("애피타이저", "시저샐러드", 8000),
-    STEAK("메인", "티본스테이크", 55000),
-    SEA_FOOD_PASTA("메인", "해산물파스타", 35000),
-    CHRIST_MAS_PASTA("메인", "크리스마스파스타", 25000),
-    CAKE("디저트", "초코케이크", 15000),
-    ICE_CREAM("디저트", "아이스크림", 5000),
-    COKE("음료", "제로콜라", 3000),
-    WINE("음료", "레드와인", 60000),
-    CHAMPAGNE("음료", "샴페인", 25000);
+    SOUP(FoodType.APPETIZER, "양송이수프", 6000),
+    TAPAS(FoodType.APPETIZER, "타파스", 5000),
+    SALAD(FoodType.APPETIZER, "시저샐러드", 8000),
+    STEAK(FoodType.MAIN, "티본스테이크", 55000),
+    SEA_FOOD_PASTA(FoodType.MAIN, "해산물파스타", 35000),
+    CHRIST_MAS_PASTA(FoodType.MAIN, "크리스마스파스타", 25000),
+    CAKE(FoodType.DESSERT, "초코케이크", 15000),
+    ICE_CREAM(FoodType.DESSERT, "아이스크림", 5000),
+    COKE(FoodType.DRINK, "제로콜라", 3000),
+    WINE(FoodType.DRINK, "레드와인", 60000),
+    CHAMPAGNE(FoodType.DRINK, "샴페인", 25000);
 
-    private String type;
+    private FoodType type;
     private String foodName;
     private int price;
 
-    MenuType(String type, String foodName, int price) {
+    MenuType(FoodType type, String foodName, int price) {
         this.type = type;
         this.foodName = foodName;
         this.price = price;
     }
 
-    public String getType() {
+    public FoodType getType() {
         return type;
     }
 
