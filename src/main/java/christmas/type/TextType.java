1fda319c1bca3ba0d3535835f4aab5b85aa26726
win9 <krrr883@gmail.com>
2023-11-13 23:59:45 +0900

    [Feat]: textType 변경
    날짜 선언부분 제거

--- src/main/java/christmas/type/TextType.java
+++ src/main/java/christmas/type/TextType.java
@@ -4,7 +4,7 @@ public enum TextType {
     START_MESSAGE("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다."),
     RESTAURANT_DATE_INPUT_MESSAGE("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)"),
     MENU_ORDER_INPUT_MESSAGE("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)"),
-    SHOW_BENEFIT_OUTPUT_MESSAGE("12월 26일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!"),
+    SHOW_BENEFIT_OUTPUT_MESSAGE("일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!"),
     ORDER_MENU_OUTPUT_MESSAGE("<주문 메뉴>"),
     BEFORE_DISCOUNT_ORDER_PRICE("<할인 전 총주문 금액>"),
     PRESENTATION_MENU("<증정 메뉴>"),
