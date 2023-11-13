a4dd58aabd541229c00b311f05e978229262b14e
Yong Ta Park <qkrdydxk80@nate.com>
2023-11-13 15:12:11 +0900

    add : 뱃지 여부 판단로직 구현

--- src/main/java/christmas/enums/badge/benefit/BenefitBadge.java
+++ src/main/java/christmas/enums/badge/benefit/BenefitBadge.java
@@ -3,9 +3,9 @@ package christmas.enums.badge.benefit;
 import christmas.enums.badge.Badge;
 
 public enum BenefitBadge implements Badge {
-    STAR("별",5_000)
+    SANTA("산타",20_000)
     , TREE("트리",10_000)
-    , SANTA("산타",20_000)
+    , STAR("별",5_000)
     , NONE("없음",0);
 
     private final String name;
@@ -14,6 +14,15 @@ public enum BenefitBadge implements Badge {
     BenefitBadge(String name, Integer benefitCondition) {
         this.name = name;
         this.benefitCondition = benefitCondition;
+    }
+
+    public static BenefitBadge determineBadge(Integer totalBenefitAmount) {
+        for (BenefitBadge badge : values()) {
+            if (totalBenefitAmount >= badge.benefitCondition) {
+                return badge;
+            }
+        }
+        return NONE;
     }
 
     @Override
