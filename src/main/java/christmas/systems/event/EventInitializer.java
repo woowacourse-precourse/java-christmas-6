a91082e08fe1c92211f123bf86594dd8461d4e4a
Yong Ta Park <qkrdydxk80@nate.com>
2023-11-14 14:25:30 +0900

    refactor: 이벤트 인터페이스들을 리스트로 가지고, 원하는 이벤트를 저장

--- src/main/java/christmas/systems/event/EventInitializer.java
+++ src/main/java/christmas/systems/event/EventInitializer.java
@@ -1,73 +1,64 @@
 package christmas.systems.event;
 
-import static christmas.enums.benefit.DiscountBenefit.BASIC_BENEFIT;
-import static christmas.enums.benefit.DiscountBenefit.GIFT_CONDITION_BENEFIT;
-import static christmas.enums.benefit.DiscountBenefit.INCREASE_BENEFIT;
-import static christmas.enums.benefit.DiscountBenefit.WEEK_BENEFIT;
-import static christmas.enums.events.decemberevent.DecemberEventPeriod.END_OF_THE_CHRISTMAS;
-import static christmas.enums.events.decemberevent.DecemberEventPeriod.MONTH;
-import static christmas.enums.events.decemberevent.DecemberEventPeriod.START_OF_THE_MONTH;
-import static christmas.enums.events.decemberevent.DecemberEventPeriod.YEAR;
-import static christmas.enums.events.decemberevent.DecemberEvents.CHRISTMAS_D_DAY_DISCOUNT;
-import static christmas.enums.events.decemberevent.DecemberEvents.SPECIAL_DISCOUNT;
-import static christmas.enums.events.decemberevent.DecemberEvents.WEEKDAY_DISCOUNT;
-import static christmas.enums.events.decemberevent.DecemberEvents.WEEKEND_DISCOUNT;
-import static christmas.enums.menu.BeverageMenu.CHAMPAGNE;
-
-import christmas.enums.menu.DessertMenu;
-import christmas.enums.menu.MainMenu;
-import christmas.enums.menu.MenuItem;
-import christmas.event.Gift;
-import christmas.event.OneEventResult;
-import christmas.event.evnets.gift.AmountToAGiftEvent;
 import christmas.event.evnets.gift.AmountToGiftEvent;
-import christmas.event.evnets.increasediscount.IncreaseDiscountUntilTypicalDay;
 import christmas.event.evnets.increasediscount.IncreaseEverydayDiscountEvent;
-import christmas.event.evnets.specialdiscount.SpecialDayDiscountEvent;
 import christmas.event.evnets.specialdiscount.SpecialDiscountEvent;
 import christmas.event.evnets.weekdiscount.WeekDiscountEvent;
-import christmas.event.evnets.weekdiscount.WeekdayDiscount;
-import christmas.event.evnets.weekdiscount.WeekendDiscount;
-import christmas.order.Orders;
-import christmas.utils.EventPeriod;
-import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 public class EventInitializer {
-    private final static Integer GIFT_QUANTITY = 1;
-    private final AmountToGiftEvent amountToGiftEvent;
-    private final IncreaseEverydayDiscountEvent increaseEverydayDiscountEvent;
-    private final SpecialDiscountEvent specialDiscountEvent;
-    private final WeekDiscountEvent weekdayDiscountEvent;
-    private final WeekDiscountEvent weekendDiscountEvent;
+    private List<IncreaseEverydayDiscountEvent> increaseEverydayDiscountEvents =new ArrayList<>();
+    private List<AmountToGiftEvent> amountToGiftEvents =new ArrayList<>();
+    private List<SpecialDiscountEvent> specialDiscountEvents =new ArrayList<>();
+    private List<WeekDiscountEvent> weekDiscountEvents =new ArrayList<>();
 
-    public EventInitializer(AmountToGiftEvent amountToGiftEvent,
-                            IncreaseEverydayDiscountEvent increaseEverydayDiscountEvent,
-                            SpecialDiscountEvent specialDiscountEvent, WeekDiscountEvent weekdayDiscountEvent,
-                            WeekDiscountEvent weekendDiscountEvent) {
-        this.amountToGiftEvent = amountToGiftEvent;
-        this.increaseEverydayDiscountEvent = increaseEverydayDiscountEvent;
-        this.specialDiscountEvent = specialDiscountEvent;
-        this.weekdayDiscountEvent = weekdayDiscountEvent;
-        this.weekendDiscountEvent = weekendDiscountEvent;
+
+    public void increaseEverydayDiscountEventsAdd(IncreaseEverydayDiscountEvent increaseEverydayDiscountEvent){
+        increaseEverydayDiscountEvents.add(increaseEverydayDiscountEvent);
     }
 
-    public OneEventResult christmasDDayDiscount(LocalDate reservationDate){
-       return increaseEverydayDiscountEvent.execute(reservationDate);
+    private void increaseEverydayDiscountEventsRemove(IncreaseEverydayDiscountEvent increaseEverydayDiscountEvent){
+        increaseEverydayDiscountEvents.remove(increaseEverydayDiscountEvent);
     }
 
-    public OneEventResult specialDayDiscountEvent(LocalDate reservationDate){
-        return specialDiscountEvent.execute(reservationDate);
+    public void amountToGiftEventsAdd(AmountToGiftEvent amountToGiftEvent){
+        amountToGiftEvents.add(amountToGiftEvent);
     }
 
-    public OneEventResult weekdayDiscount(LocalDate reservationDate, Orders orders){
-        return weekdayDiscountEvent.execute(reservationDate,orders);
+    private void amountToGiftEventsRemove(AmountToGiftEvent amountToGiftEvent){
+        amountToGiftEvents.remove(amountToGiftEvent);
     }
 
-    public OneEventResult weekendDiscount(LocalDate reservationDate, Orders orders){
-        return weekendDiscountEvent.execute(reservationDate,orders);
+    public void specialDiscountEventAdd(SpecialDiscountEvent specialDiscountEvent){
+        specialDiscountEvents.add(specialDiscountEvent);
     }
 
-    public Gift amountToAGiftEvent(LocalDate reservationDate, Integer totalPriceBeforeDiscount){
-        return amountToGiftEvent.execute(reservationDate,totalPriceBeforeDiscount);
+    private void specialDiscountEventRemove(SpecialDiscountEvent specialDiscountEvent){
+        specialDiscountEvents.remove(specialDiscountEvent);
+    }
+
+    public void weekDiscountEventAdd(WeekDiscountEvent weekDiscountEvent){
+        weekDiscountEvents.add(weekDiscountEvent);
+    }
+
+    private void weekDiscountEventRemove(WeekDiscountEvent weekDiscountEvent){
+        weekDiscountEvents.remove(weekDiscountEvent);
+    }
+
+    public List<IncreaseEverydayDiscountEvent> getIncreaseEverydayDiscountEvents() {
+        return increaseEverydayDiscountEvents;
+    }
+
+    public List<AmountToGiftEvent> getAmountToGiftEvents() {
+        return amountToGiftEvents;
+    }
+
+    public List<SpecialDiscountEvent> getSpecialDiscountEvents() {
+        return specialDiscountEvents;
+    }
+
+    public List<WeekDiscountEvent> getWeekDiscountEvents() {
+        return weekDiscountEvents;
     }
 }
