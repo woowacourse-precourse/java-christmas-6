26941033b9246cda5c825775f260c2e25f292f1b
Yong Ta Park <qkrdydxk80@nate.com>
2023-11-14 19:32:21 +0900

    refactor : 매직넘버 지역변수화

--- src/main/java/christmas/RestaurantReservation.java
+++ src/main/java/christmas/RestaurantReservation.java
@@ -28,24 +28,31 @@ public class RestaurantReservation {
     private final static Integer MONTH = Month.DECEMBER.getValue();
     private final static String RESTAURANT_NAME = "우테코 식당";
     //한달 진행이벤트
-    private final static EventPeriod monthPeriod = EventPeriod.createMonthPeriod(2023, 12);
+    private final static EventPeriod monthPeriod = EventPeriod.createMonthPeriod(YEAR, MONTH);
     //특정기간 진행 이벤트
-    private final static EventPeriod typicalPeriod = EventPeriod.createTypicalPeriod(2023, 12, 1, 25);
+    private final static EventPeriod typicalPeriod = EventPeriod.createTypicalPeriod(YEAR, MONTH, 1, 25);
     //주말/주간 할인 메뉴
     private final static MenuItem[] weekdayMenus = MainMenu.values();
     private final static MenuItem[] weekendMenus = DessertMenu.values();
 
     public RestaurantReservation() {
+        final Integer LINEAR_DISCOUNT_AMOUNT =1000;
+        final Integer SPECIAL_DISCOUNT_AMOUNT =1000;
+        final Integer INCREASE_PER_DAY = 100;
+        final Integer GIFT_CONDITION = 120_000;
+        final Integer WEEKDAY_DISCOUNT_AMOUNT = 2023;
+        final Integer WEEKEND_DISCOUNT_AMOUNT = 2023;
+
         LinerIncreaseDiscount linearDiscount = EventFactory.createLinearDiscount(CHRISTMAS_D_DAY_DISCOUNT,
-                typicalPeriod, 1000, 100);
+                typicalPeriod, LINEAR_DISCOUNT_AMOUNT, INCREASE_PER_DAY);
         SpecialDayDayDiscount specialDayDiscount = EventFactory.createSpecialDayDiscountEvent(
-                SPECIAL_DISCOUNT, monthPeriod, 1000);
-        AmountToAGiftEvent amountToAGiftEvent = EventFactory.createAmountToAGiftEvent(monthPeriod, 120_000, CHAMPAGNE,
+                SPECIAL_DISCOUNT, monthPeriod, SPECIAL_DISCOUNT_AMOUNT);
+        AmountToAGiftEvent amountToAGiftEvent = EventFactory.createAmountToAGiftEvent(monthPeriod, GIFT_CONDITION, CHAMPAGNE,
                 1);
         WeekdayDiscount weekdayDiscount = EventFactory.createWeekdayDiscount(WEEKDAY_DISCOUNT, monthPeriod,
-                weekdayMenus, 2023);
+                weekdayMenus, WEEKDAY_DISCOUNT_AMOUNT);
         WeekendDiscount weekendDiscount = EventFactory.createWeekendDiscount(WEEKEND_DISCOUNT, monthPeriod,
-                weekendMenus, 2023);
+                weekendMenus, WEEKEND_DISCOUNT_AMOUNT);
 
         EventInitializer eventInitializer = new EventInitializer();
         eventInitializer.increaseEverydayDiscountEventsAdd(linearDiscount);
