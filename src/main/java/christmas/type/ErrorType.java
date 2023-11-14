52aed5d1fa8ae5697a90d2e87cb33a0cecb78f48
win9 <krrr883@gmail.com>
2023-11-15 06:39:13 +0900

    [Feat]: 에러타입 추가
    에러메시지 추가

--- src/main/java/christmas/type/ErrorType.java
+++ src/main/java/christmas/type/ErrorType.java
@@ -1,4 +1,15 @@
 package christmas.type;
 
 public enum ErrorType {
+    NOT_VALID_ORDER("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+
+    private String text;
+
+    ErrorType(String text) {
+        this.text = text;
+    }
+
+    public String getText() {
+        return text;
+    }
 }
