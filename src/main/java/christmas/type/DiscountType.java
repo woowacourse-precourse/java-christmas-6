20e879d521f679fab7801aeee3850f39d293708e
win9 <krrr883@gmail.com>
2023-11-15 08:08:33 +0900

    [Feat]: 특별, 평일, 주말할인 구별
    평일 주말할인을 구별하여 할인

--- src/main/java/christmas/type/DiscountType.java
+++ src/main/java/christmas/type/DiscountType.java
@@ -3,6 +3,7 @@ package christmas.type;
 public enum DiscountType {
     CHRISTMAS_DISCOUNT("크리스마스 디데이 할인"),
     NORMAL_DISCOUNT("평일 할인"),
+    WEEK_DISCOUNT("주말 할인"),
     SPECIAL_DISCOUNT("특별 할인"),
     GIVING_DISCOUNT("증정 이벤트");
 
