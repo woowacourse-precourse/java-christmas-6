20fa69dca79c8c2311a612bf27472512c52be5a1
win9 <krrr883@gmail.com>
2023-11-15 20:58:10 +0900

    [Test]: 날짜 입력 예외 테스트 추가
    실제 입력에 따른 날짜 예외 테스트 추가

---
+++ src/test/java/christmas/controller/PlannerControllerTest.java
@@ -0,0 +1,38 @@
+package christmas.controller;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import christmas.Application;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class PlannerControllerTest extends NsTest {
+
+    @ParameterizedTest
+    @ValueSource(strings = {" ", "\n", "\r", "A"})
+    void 날짜_입력_예외_테스트() {
+        assertSimpleTest(() -> {
+            runException("a");
+            assertThat(output()).contains("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+        });
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"0", "-1", "32"})
+    void 날짜_범위_입력_예외_테스트() {
+        assertSimpleTest(() -> {
+            runException("a");
+            assertThat(output()).contains("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+        });
+    }
+
+
+    @Override
+    protected void runMain() {
+        Application.main(new String[]{});
+    }
+}
