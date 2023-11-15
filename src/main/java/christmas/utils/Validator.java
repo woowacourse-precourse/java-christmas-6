d887c287d15cfa0f0b0c35e551b9e393ad374d74
win9 <krrr883@gmail.com>
2023-11-15 15:43:03 +0900

    [Feat]: 날짜에 대한 에러 추가
    ErrorType을 추가하고 에러를 출력

--- src/main/java/christmas/utils/Validator.java
+++ src/main/java/christmas/utils/Validator.java
@@ -1,9 +1,16 @@
 package christmas.utils;
 
+import christmas.exception.NotValidDateException;
+import christmas.type.ErrorType;
+
 public class Validator {
+
+    public static final int DECEMBER_FIRST = 1;
+    public static final int DECEMBER_LAST = 31;
+
     public static void checkDateValid(int date) {
-        if (date < 1 || date > 31) {
-            throw new IllegalArgumentException();
+        if (date < DECEMBER_FIRST || date > DECEMBER_LAST) {
+            throw new NotValidDateException(ErrorType.NOT_VALID_DATE.getText());
         }
     }
 }
