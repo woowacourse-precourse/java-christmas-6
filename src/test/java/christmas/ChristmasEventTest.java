e8fb27755af81e247e416e878c903b5fb2c5751b
SeokEui Hong <hongs7701@naver.com>
2023-11-29 23:09:19 +0900

    refactor: [#19] ChristmasEventTest 중복 코드 제거
    1. assertDDayDiscount()
     - 디데이디스카운트의 해당 금액만큼 반환해주는지 확인해 주는 코드의 중복을 제거하기 위해 생성

--- src/test/java/christmas/ChristmasEventTest.java
+++ src/test/java/christmas/ChristmasEventTest.java
@@ -4,65 +4,40 @@ import christmas.domain.ChristmasEvent;
 import christmas.domain.OrderList;
 import christmas.domain.ReservationDate;
 import org.junit.jupiter.api.Test;
+import org.mockito.internal.matchers.Or;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ChristmasEventTest {
     @Test
     void orderBeforeDecemberTwentySixth() {
-        ReservationDate reservationDate1 = new ReservationDate(1);
-        OrderList orderList1 = new OrderList(new String[] {"해산물파스타-1"});
-        ChristmasEvent christmasEvent1 = new ChristmasEvent(orderList1, reservationDate1);
-        int resultDDayDiscount1 = christmasEvent1.calculateDDayDiscount();
-
         int expectedDDayDiscount1 = 1000;
-
-        assertEquals(expectedDDayDiscount1, resultDDayDiscount1);
-
-        ReservationDate reservationDate2 = new ReservationDate(11);
-        OrderList orderList2 = new OrderList(new String[] {"크리스마스파스타-1"});
-        ChristmasEvent christmasEvent2 = new ChristmasEvent(orderList2, reservationDate2);
-        int resultDDayDiscount2 = christmasEvent2.calculateDDayDiscount();
-
+        assertDDayDiscount(new OrderList(new String[] {"해산물파스타-1"}), new ReservationDate(1), expectedDDayDiscount1);
         int expectedDDayDiscount2 = 2000;
+        assertDDayDiscount(new OrderList(new String[] {"크리스마스파스타-1"}), new ReservationDate(11), expectedDDayDiscount2);
+    }
 
-        assertEquals(expectedDDayDiscount2, resultDDayDiscount2);
+    private void assertDDayDiscount(OrderList orderList, ReservationDate reservationDate, int expectedDiscount) {
+        ChristmasEvent christmasEvent = new ChristmasEvent(orderList, reservationDate);
+        int realDiscount = christmasEvent.calculateDDayDiscount();
+        assertEquals(expectedDiscount, realDiscount);
     }
 
     @Test
     void orderAfterChristmas() {
-        ReservationDate reservationDate1 = new ReservationDate(26);
-        OrderList orderList1 = new OrderList(new String[] {"해산물파스타-1"});
-        ChristmasEvent christmasEvent1 = new ChristmasEvent(orderList1, reservationDate1);
-        int resultDDayDiscount1 = christmasEvent1.calculateDDayDiscount();
-
-        int expectedDDayDiscount1 = 0;
-
-        assertEquals(expectedDDayDiscount1, resultDDayDiscount1);
+        int expectedDDayDiscount = 0;
+        assertDDayDiscount(new OrderList(new String[] {"해산물파스타-1"}), new ReservationDate(26), expectedDDayDiscount);
     }
 
     @Test
     void orderMoreThanTenThousandBeforeChristmas() {
-        ReservationDate reservationDate1 = new ReservationDate(1);
-        OrderList orderList1 = new OrderList(new String[] {"해산물파스타-1"});
-        ChristmasEvent christmasEvent1 = new ChristmasEvent(orderList1, reservationDate1);
-        int resultDDayDiscount1 = christmasEvent1.calculateDDayDiscount();
-
         int expectedDDayDiscount1 = 1000;
-
-        assertEquals(expectedDDayDiscount1, resultDDayDiscount1);
+        assertDDayDiscount(new OrderList(new String[] {"해산물파스타-1"}), new ReservationDate(1), expectedDDayDiscount1);
     }
 
     @Test
     void orderUnderTenThousandBeforeChristmas() {
-        //TO DO 1 : Ddaycountodown 이벤트 마지막 기능 이벤총주문 금액 10000원 이하에서만 작동하는지 확인
-        ReservationDate reservationDate1 = new ReservationDate(1);
-        OrderList orderList1 = new OrderList(new String[] {"타파스-1"});
-        ChristmasEvent christmasEvent1 = new ChristmasEvent(orderList1, reservationDate1);
-        int resultDDayDiscount1 = christmasEvent1.calculateDDayDiscount();
-
         int expectedDDayDiscount1 = 0;
-
-        assertEquals(expectedDDayDiscount1, resultDDayDiscount1);
+        assertDDayDiscount(new OrderList(new String[] {"타파스-1"}), new ReservationDate(1), expectedDDayDiscount1);
     }
 }
