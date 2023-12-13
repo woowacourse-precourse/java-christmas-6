d52a9ac2aa52eb73c6727c965f128464f70a1113
zangsu <zangsu_@naver.com>
2023-12-13 17:12:27 +0900

    refactor : 사용하지 않는 클래스 삭제

--- src/main/java/christmas/exception/PromotionExceptionMaker.java
+++ src/main/java/christmas/exception/PromotionExceptionMaker.java
@@ -6,12 +6,8 @@ public enum PromotionExceptionMaker {
 
     BLANK_INPUT("입력값이 비어 있습니다."),
     NO_SUCH_MENU("잘못된 메뉴 입력입니다."),
-    MENU_AMOUNT_MUST_POSITIVE("상품 수량은 양수여야 합니다."),
-    MONEY_MUST_NOT_NEGATIVE("금액은 0 이상이어야 합니다."),
-    NOT_ENOUGH_MONEY("잔액이 부족합니다."),
     CANT_APPLY_BENEFIT_EXCEPTION("혜택을 적용할 수 없습니다."),
     INVALID_DATE("유효하지 않은 날짜입니다."),
-    EMPTY_ORDER("주문이 비어있습니다."),
     DUPLICATE_ORDER("중복된 주문이 있습니다."),
     TOO_MANY_ORDERS("주문은 20개 이하로 가능합니다."),
     ALL_ORDER_DRINK("주문은 음료만으로는 불가능합니다."),
